45bf99bf8ffc2a80ea149e8de39dd79213380762
davian-007 <soloaprogramacion@gmail.com>
2016-06-01 14:11:50 -0600

    Commit inicial

---
+++ CA4/Book.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA4
+{
+    class Book
+    {
+        private int bookid;
+        private string bookname;
+        private string bookauthor;
+        private double bookprice;
+        public double size;
+
+        private struct book
+        {
+            public int bookid;
+            public string bookname;
+            public string bookauthor;
+            public double bookprice;
+        };
+
+        private static book[] books;
+
+        //constructor
+        public Book()
+        {
+            bookid = 0;
+            bookname = "";
+            bookauthor = "";
+            bookprice = 0;
+        }
+
+        //contructor con parámetros
+        public Book(int id, string name, string author, double price)
+        {
+            bookid = id;
+            bookname = name;
+            bookauthor = author;
+            bookprice = price;
+        }
+
+        //propiedades 
+        /*public int BookID
+        {
+            get { return bookid; }
+            set { bookid = value; }
+        }
+
+        public string BookName
+        {
+            get { return bookname; }
+            set { bookname = value; }
+        }*/
+
+        //propiedades 
+        public void SetBookID(int id)
+        {
+            bookid = id;
+        }
+
+        public int GetBookID()
+        {
+            return bookid;
+        }
+
+        public void SetBookName(string name)
+        {
+            bookname = name;
+        }
+
+        public string GetBookName()
+        {
+            return bookname;
+        }
+
+        //Pendientes propiedades de autor y precio
+
+        public void SetSize(int Size)
+        {
+            size = Size;
+            books = new book[Size];
+        }
+
+        public void Initdata()
+        {
+            //inicializar
+            for (int i = 0; i < books.Length; i++)
+            {
+                books[i].bookid = 0;
+                books[i].bookname = "";
+                books[i].bookauthor = "";
+                books[i].bookprice = 0;
+            }
+        }
+
+        public bool AddData(Book Book)
+        {
+            //agregar datos
+            int i;
+            for (i = 0; i < books.Length; i++)
+            {
+                if (books[i].bookid == 0)
+                {
+                    break;
+                }
+            }
+
+            if (i < books.Length)
+            {
+                books[i].bookid = Book.bookid;
+                books[i].bookname = Book.bookname;
+                books[i].bookauthor = Book.bookauthor;
+                books[i].bookprice = Book.bookprice;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool SeekData(int record)
+        {
+            //buscar datos
+            int i;
+            for (i = 0; i < books.Length; i++)
+            {
+                if (books[i].bookid == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < books.Length)
+            {
+                bookid = books[i].bookid;
+                bookname = books[i].bookname;
+                bookauthor = books[i].bookauthor;
+                bookprice = books[i].bookprice;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateData(Book Book, int record)
+        {
+            //actualizar datos
+            int i;
+            for (i = 0; i < books.Length; i++)
+            {
+                if (books[i].bookid == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < books.Length)
+            {
+                books[i].bookid = Book.bookid;
+                books[i].bookname = Book.bookname;
+                books[i].bookauthor = Book.bookauthor;
+                books[i].bookprice = Book.bookprice;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteData(int record)
+        {
+            //borrar datos
+            int i;
+            for (i = 0; i < books.Length; i++)
+            {
+                if (books[i].bookid == record)
+                {
+                    break;
+                }
+            }
+
+            if (i < books.Length)
+            {
+                books[i].bookid = 0;
+                books[i].bookname = "";
+                books[i].bookauthor = "";
+                books[i].bookprice = 0;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void PrintData(int pos)
+        {
+            //imprimir todo el registro
+            bookid = books[pos].bookid;
+            bookname = books[pos].bookname;
+            bookauthor = books[pos].bookauthor;
+            bookprice = books[pos].bookprice;
+        }
+
+        // destructor
+        ~Book()
+        {
+
+        }
+    }
+}
---
+++ CA4/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+
+namespace CA4
+{
+    class Program
+    {
+        static Book Libro = new Book();
+
+        static int option = 0;
+        static int size = 3;
+        [Flags]
+        enum BookPrice
+        {
+            Bajo = 500,
+            MedioBajo = 2500,
+            Medio = 5000,
+            MedioAlto = 7500,
+            Alto = 10000,
+        };
+
+        static void AddBooks()
+        {
+            //agregar libro
+            int r = 0, i= 0;
+            while (r != 1)
+            {
+                Console.Clear();
+                if (i < Libro.size)
+                {
+                    Console.WriteLine("-* Librería el Buen Lector  *-*");
+                    Console.WriteLine("Ingreso de libros.             ");
+                    Console.Write("Digite el ID:                  ");
+                    Libro.SetBookID(int.Parse(Console.ReadLine()));
+                    Console.Write("Digite el nombre del libro:    ");
+                    Libro.SetBookName(Console.ReadLine());
+                    /*Console.Write("Digite el autor del libro:     ");
+                    Console.ReadLine();
+                    Console.WriteLine("Seleccione el precio del libro:");
+                    Console.Write(BookPrice.Bajo + ":" + (int)BookPrice.Bajo + "\n" +
+                    BookPrice.MedioBajo + ":" + (int)BookPrice.MedioBajo + "\n" +
+                    BookPrice.Medio + ":" + (int)BookPrice.Medio + "\n" +
+                    BookPrice.MedioAlto + ":" + (int)BookPrice.MedioAlto + "\n" +
+                    BookPrice.Alto + ":" + (int)BookPrice.Alto + "\n");
+                    double.Parse(Console.ReadLine());*/
+                    if (Libro.AddData(Libro) == true)
+                        Console.WriteLine("Libro agregado correctamente");
+                    else
+                        Console.WriteLine("Libro no agregado");
+                    Console.WriteLine("Desea agregar otro libro 0-Sí 1-No");
+                    r = int.Parse(Console.ReadLine());
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("Registro de libros lleno..");
+                    r = 1;
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+        static void ShowBooks()
+        {
+            //listar libros
+            int reg = 0, i;
+            Console.Clear();
+            Console.WriteLine("-* Librería el Buen Lector  *-*");
+            Console.WriteLine("Listado de libros.             ");
+            for (i = 0; i < Libro.size; i++)
+            {
+                Libro.PrintData(i);
+                reg = i + 1;
+                Console.WriteLine("Registro: " + reg);
+                Console.WriteLine("ID:       " + Libro.GetBookID());
+                Console.WriteLine("Nombre:   " + Libro.GetBookName());
+                /*Console.WriteLine("Auto:     " + );
+                Console.WriteLine("Precio:   " + );*/
+            }
+            Console.ReadKey();
+        }
+
+        static void SeekBooks()
+        {
+            //buscar libro
+            int ID = 0, reg = 0, r = 0;
+            while (r != 1)
+            {
+                Console.Clear();
+                Console.WriteLine("-* Librería el Buen Lector  *-*");
+                Console.WriteLine("Búsquedad de libros.           ");
+                Console.WriteLine("Digite el ID del libro a buscar");
+                ID = int.Parse(Console.ReadLine());
+                if (Libro.SeekData(ID) == true)
+                {
+                    reg = ID;
+                    Console.WriteLine("Registro: " + reg);
+                    Console.WriteLine("ID:       " + Libro.GetBookID());
+                    Console.WriteLine("Nombre:   " + Libro.GetBookName());
+                    /*Console.WriteLine("Auto:     " + );
+                    Console.WriteLine("Precio:   " + );*/
+                }
+                else
+                    Console.WriteLine("No se encontro ese ID");
+                Console.WriteLine("Desea buscar otro libro 0-Sí 1-No");
+                r = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void PrintInvoice()
+        {
+            string currentPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName);
+            Console.WriteLine(currentPath + @"\Invoice.txt");
+
+            StreamWriter sw = new StreamWriter(currentPath + @"\Invoice.txt");
+            //Imprimiendo la factura
+            sw.WriteLine("===================================================================");
+            sw.WriteLine("*-*                  Librería El Buen Lector                    *-*");
+            sw.WriteLine("===================================================================");
+            sw.WriteLine(" Factura Proforma\t\t\t\t\t    N°0001");
+            sw.Write(" Cliente : ");
+            sw.WriteLine("Jorge Cordoba");
+            sw.Write(" Teléfono: ");
+            sw.WriteLine("88775599");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.WriteLine(" Item\t\t\t\tCtd\tPrecio\t\tSubTotal");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" El Señor de los anillos, las..");
+            sw.Write("\t" + "1");
+            sw.Write("\t¢" + "4999,99");
+            sw.WriteLine("\t¢" + "4999.99");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" Total Orden:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "4999.99");
+            sw.Write(" Total Descuento:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "499,99");
+            sw.Write(" Impuesto Venta: \t\t\t\t\t");
+            sw.WriteLine("13" + "%");
+            sw.Write(" Impuesto Total: \t\t\t\t\t");
+            sw.WriteLine("¢" + "539.99");
+            sw.Write(" Precio Neto:    \t\t\t\t\t");
+            sw.WriteLine("¢" + "4999,99");
+            sw.WriteLine("-------------------------------------------------------------------");
+            sw.Write(" Monto Recibido:");
+            sw.WriteLine("¢" + "50000");
+            sw.Write(" Monto Cambio  :");
+            sw.WriteLine("¢" + "1");
+            sw.WriteLine("===================================================================");
+            sw.Write(" Gracias por comprar con nosotros...");
+            sw.Close();
+        }
+
+        static void Main()
+        {
+            //inicializar el objeto
+            Libro.SetSize(size);
+            Libro.Initdata();
+
+            PrintInvoice();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("-* Librería el Buen Lector  *-*");
+                Console.WriteLine("1. Ingreso de libros.");
+                Console.WriteLine("2. Modificación de libros.");
+                Console.WriteLine("3. Eliminación de libros.");
+                Console.WriteLine("4. Búsquedad de libros.");
+                Console.WriteLine("5. Listado de libros.");
+                Console.WriteLine("6. Venta de libros.");
+                Console.WriteLine("7. Salir.");
+                Console.WriteLine("Selecciones una opción");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        {                            
+                            AddBooks();
+                            break;
+                        }
+                    case 2:
+                        {
+                            //Modificar la información
+                            break;
+                        }
+                    case 3:
+                        {
+                            //Eliminar información dejando campos en 0 o vacío
+                            break;
+                        }
+                    case 4:
+                        {
+                            SeekBooks();
+                            break;
+                        }
+                    case 5:
+                        {
+                            ShowBooks();
+                            break;
+                        }
+                    case 6:
+                        {
+                            //Venta similar a tarea1
+                            break;
+                        }
+                    case 7:
+                        //Salir
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Seleccione inválida..");
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
+                        }
+                }
+            } while (option != 7);
+        }
+    }
+}
